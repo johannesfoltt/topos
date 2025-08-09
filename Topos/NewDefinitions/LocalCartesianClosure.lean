e63cb080e5099758e6f1d7d3b20f480308b3eb64
fohannesj <johannesfolttmann@gmail.com>
2025-08-09 17:04:18 +0200

    OverOverEquiv

--- Topos/NewDefinitions/LocalCartesianClosure.lean
+++ Topos/NewDefinitions/LocalCartesianClosure.lean
@@ -4,7 +4,7 @@ import Mathlib.CategoryTheory.Monoidal.Cartesian.Over
 
 namespace CategoryTheory.Over
 
-open Category Limits Over MonoidalCategory CartesianMonoidalCategory CartesianClosed Adjunction
+open Category Functor Limits Over MonoidalCategory CartesianMonoidalCategory CartesianClosed Adjunction
 
 universe u v
 
@@ -15,8 +15,6 @@ noncomputable section
 local instance cartesianMonoidalCategory_base : CartesianMonoidalCategory C := ofHasFiniteProducts
 
 variable {X : C} [Exponentiable X]
-
---local instance cartesianMonoidalCategory_Over : CartesianMonoidalCategory (Over X) := cartesianMonoidalCategory X
 
 lemma HomEq_toFun_w {A : C} {B : Over X} (f : (star X).obj A ⟶ B) : CartesianClosed.curry (f.left) ≫
 ((exp X).map B.hom) = (toUnit A) ≫ (internalizeHom (𝟙 X)) := by {
@@ -77,4 +75,84 @@ def coreHomEq : Adjunction.CoreHomEquiv (star X) (Γ X) where
     · simp
   }
 
-def starΓadj : (star X) ⊣ (Γ X) := Adjunction.mkOfHomEquiv coreHomEq
+def starAdjΓ : (star X) ⊣ (Γ X) := Adjunction.mkOfHomEquiv coreHomEq
+
+variable {X' : C} (f : X' ⟶ X)
+
+def eqvOverOver_functor : Over (mk f) ⥤ Over X' where
+  obj := fun A => mk A.hom.left
+  map := fun g => homMk g.left.left (by rw [← (Over.w g)]; simp)
+
+def eqvOverOver_inverse : Over X' ⥤ Over (mk f) where
+  obj := fun A => mk (homMk A.hom : mk (A.hom ≫ f) ⟶ mk f)
+  map := fun g => homMk (homMk g.left)
+
+def equvOverOver_unitIso_eq_components (A : Over (mk f)) : (𝟭 (Over (mk f))).obj A = (eqvOverOver_functor f ⋙ eqvOverOver_inverse f).obj A := by {
+  unfold eqvOverOver_functor
+  unfold eqvOverOver_inverse
+  simp
+  sorry
+}
+
+def equvOverOver_counitIso_eq_components (A : Over X') : (eqvOverOver_inverse f ⋙ eqvOverOver_functor f).obj A = (𝟭 (Over X')).obj A := by {
+  unfold eqvOverOver_functor
+  unfold eqvOverOver_inverse
+  simp
+  sorry
+}
+
+def eqvOverOver : Over (mk f) ≌ Over X' where
+  functor := eqvOverOver_functor f
+  inverse := eqvOverOver_inverse f
+  unitIso := NatIso.ofComponents (fun A => (eqToIso (equvOverOver_unitIso_eq_components f A)))
+  counitIso := NatIso.ofComponents (fun A => (eqToIso (equvOverOver_counitIso_eq_components f A)))
+  functor_unitIso_comp := by {
+    intros X
+    unfold eqvOverOver_functor
+    unfold eqvOverOver_inverse
+    simp
+    rw [OverMorphism.ext_iff, homMk_left _ _]
+    simp
+  }
+
+/-
+
+instance eqvOverOver_Faithful : (eqvOverOver f).Faithful where
+  map_injective := by {
+    intros A B g g' h
+    apply OverMorphism.ext ∘ OverMorphism.ext
+    unfold eqvOverOver at h; simp at h
+    rw [OverMorphism.ext_iff, homMk_left _ _, homMk_left _ _] at h
+    exact h
+  }
+
+instance eqvOverOver_Full : (eqvOverOver f).Full where
+  map_surjective := by {
+    intros A B g
+    unfold eqvOverOver at g; simp at g
+    unfold eqvOverOver; simp
+    refine Exists.intro ?_ ?_
+    · refine homMk ?_ ?_
+      · refine homMk ?_ ?_
+        · exact g.left
+        · have Ov_g := Over.w g; rw [mk_hom, mk_hom] at Ov_g
+          have Ov_A := Over.w A.hom; simp at Ov_A
+          have Ov_B := Over.w B.hom; simp at Ov_B
+          rw [← Ov_B, ← assoc, Ov_g, Ov_A]
+      · rw [OverMorphism.ext_iff, comp_left, homMk_left _ _]
+        have Ov_g := Over.w g; rw [mk_hom, mk_hom] at Ov_g
+        exact Ov_g
+    · aesop_cat
+  }
+
+instance eqvOverOver_EssSurj : (eqvOverOver f).EssSurj where
+  mem_essImage := by {
+    intro Y
+    use mk (homMk Y.hom : mk (Y.hom ≫ f) ⟶ mk f)
+    apply Nonempty.intro
+    apply Iso.refl
+  }
+
+instance eqvOverOver_IsEquivalence : (eqvOverOver f).IsEquivalence := {faithful := eqvOverOver_Faithful f, full := eqvOverOver_Full f, essSurj := eqvOverOver_EssSurj f}
+
+-/
